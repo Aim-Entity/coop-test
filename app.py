0274f61825562c61a8a4314d42ac78492e266000
Aim-Entity <59455267+Aim-Entity@users.noreply.github.com>
2022-02-16 15:13:32 +0000

    pre player move delete

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import random
 
 class GridGame:
     def __init__(self):
+        self.moves = 0
+
         self.gameMap = [["", "", "", "", "", "", "", ""],
                         ["", "", "", "", "", "", "", ""],
                         ["", "", "", "", "", "", "", ""],
@@ -52,40 +54,79 @@ class GridGame:
                 self.gameMap[y][x] = "Bandit"
                 banditDeplete -= 1
 
-    def displayMapNeatly(self):
+    def displayMapNeatly(self, gameMap):
         for i in gameMap:
             print(i)
 
-    def piecePosition(self, piece):
+    def piecePosition(self, piece, gameMap):
         for i in range(len(gameMap)):
             for j in range(len(gameMap[i])):
                 if gameMap[i][j] == piece:
                     return i, j
 
-        return False
+        return (7, 0)
 
     def playerMove(self):
         xTransformation = int(
             input("Enter number of tiles to move right(+) or left(-): "))
         yTransformation = int(
-            input("Enter number of tiles to move up(+) or down(-):"))
-        realY, realX = piecePosition("player", gameMap)
+            input("Enter number of tiles to move down(+) or up(-):"))
 
-        while (xTransformation < 1 or xTransformation > 8) or (yTransformation < 1 or yTransformation > 8):
+        currentY, currentX = self.piecePosition("Player", self.hiddenMap)
+
+        while(xTransformation < -8 or xTransformation > 8)or(yTransformation < -8 or yTransformation > 8):
             print("\nInvalid input")
             xTransformation = int(
                 input("Enter number of tiles to move right(+) or left(-): "))
             yTransformation = int(
-                input("Enter number of tiles to move up(+) or down(-):"))
+                input("Enter number of tiles to move down(+) or up(-):"))
+
+        while True:
+            newY = currentY + yTransformation
+            newX = currentX + xTransformation
+
+            if (newY < 0 or newY > 7) or (newX < 0 or newX > 7):
+                print("\nCoordinates fall out of range of map", (newY, newX))
+                xTransformation = int(
+                    input("Enter number of tiles to move right(+) or left(-): "))
+                yTransformation = int(
+                    input("Enter number of tiles to move down(+) or up(-):"))
+
+            else:
+                break
+
+        self.hiddenMap[currentY][currentX] = ""
+        self.hiddenMap[newY][newX] = "Player"
+
+        newPiece = self.gameMap[newY][newX]
+
+        if newPiece == "":
+            self.gameMap[newY][newX] = "Player"
+        else:
+            self.gameMap[newY][newX] = f"{newPiece}|Player"
+
+        if len(self.gameMap[newY][newX].split("|")) == 2:
+            if self.gameMap[newY][newX].split("|")[0] != "":
+                currentPiece = self.gameMap[newY][newX].split("|")[0]
+            else:
+                currentPiece = ""
+        else:
+            currentPiece = ""
+
+        print(self.gameMap[newY][newX].split("|"))
+        print(currentPiece)
+        print(newPiece)
+
+        self.gameMap[currentY][currentX] = currentPiece
+        self.moves += 1
 
     def gameinit(self):
-        hidden_map = [["", "", "", "", "", "", "", ""],
-                      ["", "", "", "", "", "", "", ""],
-                      ["", "", "", "", "", "", "", ""],
-                      ["", "", "", "", "", "", "", ""],
-                      ["", "", "", "", "", "", "", ""],
-                      ["", "", "", "", "", "", "", ""],
-                      ["", "", "", "", "", "", "", ""],
-                      ["Player", "", "", "", "", "", "", ""], ]
-        generated_map = self.generateMap()
-        print(self.piecePosition("Player", hidden_map))
+        self.generateMap()
+
+        while True:
+            self.displayMapNeatly(self.gameMap)
+            self.playerMove()
+
+
+G1 = GridGame()
+G1.menu()
